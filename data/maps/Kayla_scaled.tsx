b31383da3a2d194f6c07b548c36536cbfb3dfce5
Aaron Shelley <amshelley86@gmail.com>
2018-04-20 22:00:14 -0400

    Move resources into a data directory.

---
+++ data/maps/Kayla_scaled.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Kayla_scaled" tilewidth="32" tileheight="32" tilecount="256" columns="8">
+ <image source="../resources/Fantasy Character Sprites &amp; Battlers/Kayla_scaled.png" width="256" height="256"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ data/maps/kenny_roguelike_scaled.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="kenny_roguelike_scaled" tilewidth="32" tileheight="32" spacing="2" tilecount="1767" columns="57">
+ <image source="../resources/spritesheets/kenny_roguelike_scaled.png" width="1936" height="1052"/>
+ <terraintypes>
+  <terrain name="Grass" tile="5"/>
+  <terrain name="Water" tile="60"/>
+ </terraintypes>
+ <tile id="2" terrain="0,0,0,1"/>
+ <tile id="3" terrain="0,0,1,1"/>
+ <tile id="4" terrain="0,0,1,0"/>
+ <tile id="5" terrain="0,0,0,0"/>
+ <tile id="57" terrain="1,1,1,0"/>
+ <tile id="58" terrain="1,1,0,1"/>
+ <tile id="59" terrain="0,1,0,1"/>
+ <tile id="60" terrain="1,1,1,1"/>
+ <tile id="61" terrain="1,0,1,0"/>
+ <tile id="62" terrain="0,0,0,0"/>
+ <tile id="114" terrain="1,0,1,1"/>
+ <tile id="115" terrain="0,1,1,1"/>
+ <tile id="116" terrain="0,1,0,0"/>
+ <tile id="117" terrain="1,1,0,0"/>
+ <tile id="118" terrain="1,0,0,0"/>
+</tileset>
