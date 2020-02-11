4c40a64d5d20f30ebd1845a6541423742d78fcd8
Aaron Shelley <amshelley86@gmail.com>
2020-02-10 19:57:22 -0500

    Begin replacing plugin loading with static invocation.

--- src/entity/Plugin.cpp
+++ src/entity/Plugin.cpp
@@ -7,8 +7,12 @@
 #include <age/entity/Export.h>
 #include <age/entity/SelectionComponent.h>
 #include <age/entity/TransformComponent.h>
+#include <age/utilities/StaticInvoke.h>
+#include <age/utilities/Reflection.h>
 
-AGE_ENTITY_EXPORT_C void FactoryRegister()
+using namespace age::utilities;
+
+STATIC_INVOKE
 {
 	AgeFactoryRegister(age::entity::Entity);
 	AgeFactoryRegister(age::entity::EntityDatabase);
@@ -17,4 +21,21 @@ AGE_ENTITY_EXPORT_C void FactoryRegister()
 	AgeFactoryRegister(age::entity::HealthComponent).addBaseType<age::entity::Component>();
 	AgeFactoryRegister(age::entity::AnimationComponent).addBaseType<age::entity::Component>();
 	AgeFactoryRegister(age::entity::CloneSystem).addBaseType<age::entity::System>();
+
+	auto& refl = Reflection::Add<age::entity::SelectionComponent>("SelectionComponent");
+	refl.addBase<age::entity::Component>();
+	refl.addProperty("Selected", &age::entity::SelectionComponent::Selected);
+
+	refl = Reflection::Add<age::entity::TransformComponent>("TransformComponent");
+	refl.addBase<age::entity::Component>();
+	refl.addProperty("Position", &age::entity::TransformComponent::Position);
+	refl.addProperty("Rotation", &age::entity::TransformComponent::Rotation);
+
+	refl = Reflection::Add<age::entity::HealthComponent>("HealthComponent");
+	refl.addBase<age::entity::Component>();
+	refl.addProperty("HealthMax", &age::entity::HealthComponent::HealthMax);
+	refl.addProperty("Health", &age::entity::HealthComponent::Health);
+
+	refl = Reflection::Add<age::entity::AnimationComponent>("AnimationComponent");
+	refl.addBase<age::entity::Component>();
 }
